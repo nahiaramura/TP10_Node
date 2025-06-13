15128316014e70fe0f0a39014f244c7ce9675a8b
Android Studio <devandroid@ort>
2025-06-13 11:37:17 -0300

    conexion supabase

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -1,8 +1,8 @@
 const config = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Alumnos',
-    password: 'root',
+    user: 'postgres.duhluatfqajyyemuefwy',
+    host: 'aws-0-sa-east-1.pooler.supabase.com',
+    database: 'postgres',
+    password: 'nahiaramuracontra',
     port: 5432,
   };  
 

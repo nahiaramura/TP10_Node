1e5981e7cfe5978c1fbe2df16bb8373a529c249f
Android Studio <devandroid@ort>
2025-06-30 08:23:26 -0300

    mejoras

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 const DBconfig = {
   user: process.env.DB_USER ?? '',
   host: process.env.DB_HOST ?? '',
-  database: process.env.DB_NAME ?? '',
+  database: process.env.DB_DATABASE ?? '',
   password: process.env.DB_PASSWORD ?? '',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
 };
